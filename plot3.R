e15577e1c49fd772191b08284f2b9e66cb826ab1
VirajGhorpade <virajvg@gmail.com>
2014-07-11 14:46:28 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,15 @@
+#Read the file
+entiredata <- read.table("household_power_consumption.txt", sep = ";", header = T, na.strings = "?")
+
+#convert the date-time to standard format
+entiredata$Datetime <- strptime(paste(entiredata$Date, entiredata$Time), "%d/%m/%Y %H:%M:%S")
+
+#operate only on the required part of the data
+partdata <- subset(entiredata, as.character(entiredata$Date)== "1/2/2007")
+subdata <- rbind(partdata,subset(entiredata, as.character(entiredata$Date)== "2/2/2007"))
+data <- subdata
+rm(entiredata)
+
 #----------------------------------------------------
 png(file="plot3.png", width=480, height=480)
 plot(data$Datetime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
